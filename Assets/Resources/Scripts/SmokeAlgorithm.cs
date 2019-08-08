8aa320fad54557c608dacd2979404e7436877414
SonOfMondrian <modern5607@naver.com>
2019-08-08 17:27:26 +0900

    수면탄 구현 50%
    수면탄 아직 미완성

---
+++ Assets/Resources/Scripts/SmokeAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeAlgorithm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void DestroySmoke()
+    {
+        Destroy(this.gameObject);
+    }
+}
