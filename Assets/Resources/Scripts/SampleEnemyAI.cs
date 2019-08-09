65c885db873d4fbad15ce3bb829cdb67756a1bc9
kimgaejin <kimgaejin@naver.com>
2019-08-10 01:05:22 +0900

    플랫폼 변경
    *5*6? 에서 3*3으로 변경
    -벽삭제
    *블록크기 0.8에서 1로 변경

--- Assets/Resources/Scripts/SampleEnemyAI.cs
+++ Assets/Resources/Scripts/SampleEnemyAI.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class SampleEnemyAI : MonoBehaviour
 {
+    //===============================
+    // 조절 가능한 변수들 (스피드, 쿨타임, 범위 등)
     public float rangeOfPerceivingPlayer = 5.0f;
     public float movingSpeed = 2.0f;
     public float attackSpeed = 0.5f;
     public float attackCooltime = 0.0f;
     //private float attackPower = 10.0f;
+    public float jumpingPower = 5.0f;
 
+    //================================
+    // 조절 불가능한 변수
     private GameObject player;
+    private Rigidbody rigid;
     private bool isFoundPlayer = false;
+    private int jumpCount = 0;
+    private int jumpCountMax = 1;
 
     private void Start()
     {
+        rigid = transform.GetComponent<Rigidbody>();
+
         player = GameObject.Find("Player").gameObject;
+
+        jumpCount = jumpCountMax;
     }
 
     private void Update()
@@ -56,5 +68,14 @@ public class SampleEnemyAI : MonoBehaviour
         Vector3 distance = player.transform.position - transform.position;
 
         transform.position += new Vector3(distance.x, 0, 0).normalized * movingSpeed * Time.deltaTime;
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (jumpCount <= 0) return;
+
+        rigid.AddForce(Vector3.up * jumpingPower, ForceMode.Impulse);
+        jumpCount--;
     }
 }
