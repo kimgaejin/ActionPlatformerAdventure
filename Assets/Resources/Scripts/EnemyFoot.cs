bc6f6a6e702149f903584dce29d88e6ab6f01a6e
kimgaejin <kimgaejin@naver.com>
2019-08-11 17:35:37 +0900

    몬스터 연막 AI
    +몬스터가 연막 안에 들어가면 플레이어 추격X
    +몬스터가 플레이어로부터 멀어지면 플레이어 추격X
    +몬스터가 벽 너머의 플레이어는 추격X
    +몬스터가 점프하면서 옴

---
+++ Assets/Resources/Scripts/EnemyFoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFoot : MonoBehaviour
+{
+    private SampleEnemyAI ai;
+
+    private void Awake()
+    {
+        ai = transform.parent.GetComponent<SampleEnemyAI>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+            ai.OnPlatform();
+        }
+    }
+}
--- Assets/Resources/Scripts/SampleEnemyAI.cs
+++ Assets/Resources/Scripts/SampleEnemyAI.cs
@@ -15,6 +15,14 @@ public class SampleEnemyAI : MonoBehaviour
 
     //================================
     // 조절 불가능한 변수
+    private bool perceiveSomething = false;
+    private bool sightAnomaly = false;
+    private bool mindAnomaly = false;
+    private bool behaviroAnomaly = false;
+    private bool earAnomaly = false;
+
+    private Vector3 sightTarget;
+
     private GameObject player;
     private Rigidbody rigid;
     private bool isFoundPlayer = false;
@@ -28,19 +36,22 @@ public class SampleEnemyAI : MonoBehaviour
         player = GameObject.Find("Player").gameObject;
 
         jumpCount = jumpCountMax;
+        sightTarget = transform.position;
     }
 
     private void Update()
     {
-        PerceivePlayer();
+        //PerceivePlayer();
     }
 
     private void FixedUpdate()
     {
-        if (isFoundPlayer)
-        {
-            PursuePlayer();
-        }
+        Smoke();
+        if (sightAnomaly)
+            Debug.Log("sightAnomaly");
+        SeeAround();
+        
+        PursueTarget(sightTarget);
     }
 
     private IEnumerator Execution()
@@ -71,6 +82,72 @@ public class SampleEnemyAI : MonoBehaviour
         Jump();
     }
 
+    // 타겟 추적
+    private void PursueTarget(Vector3 targetVector3)
+    {
+        if (perceiveSomething == false) return;
+
+        Vector3 distance = targetVector3 - transform.position;
+
+        transform.position += new Vector3(distance.x, 0, 0).normalized * movingSpeed * Time.deltaTime;
+        Jump();
+
+        // 마지막으로 발견한 대상의 위치까지 도달했으면 추적 중단.
+        if ((targetVector3 - transform.position).magnitude < 0.5f) perceiveSomething = false;
+    }
+
+    private void SelectTarget(Transform trans)
+    {
+
+        if (trans.tag == "Player")
+        {
+            perceiveSomething = true;
+            sightTarget = trans.position;
+        }
+    }
+
+    private void SeeAround()
+    {
+        if (sightAnomaly == true)
+        {
+            perceiveSomething = false;
+            sightTarget = transform.position;
+            return;
+        }
+
+        float range = 3f;
+        Vector3 center = transform.position + new Vector3(0, 0.5f, 0);
+
+        RaycastHit hit;
+        Debug.DrawRay(center, transform.right * range, Color.blue, 0.3f);
+        Debug.DrawRay(center, -transform.right * range, Color.blue, 0.3f);
+        Debug.DrawRay(center, transform.up * range, Color.blue, 0.3f);
+        Debug.DrawRay(center, (transform.up + transform.right) * range, Color.blue, 0.3f);
+        Debug.DrawRay(center, (transform.up - transform.right) * range, Color.blue, 0.3f);
+
+        if (Physics.Raycast(center, transform.right, out hit, range))
+        {
+            SelectTarget(hit.transform);
+        }
+        if (Physics.Raycast(center, -transform.right, out hit, range))
+        {
+            SelectTarget(hit.transform);
+        }
+        if (Physics.Raycast(center, transform.up, out hit, range))
+        {
+            SelectTarget(hit.transform);
+        }
+        if (Physics.Raycast(center, transform.up + transform.right, out hit, range))
+        {
+            SelectTarget(hit.transform);
+        }
+        if (Physics.Raycast(center, transform.up - transform.right, out hit, range))
+        {
+            SelectTarget(hit.transform);
+        }
+
+    }
+
     private void Jump()
     {
         if (jumpCount <= 0) return;
@@ -78,4 +155,45 @@ public class SampleEnemyAI : MonoBehaviour
         rigid.AddForce(Vector3.up * jumpingPower, ForceMode.Impulse);
         jumpCount--;
     }
+
+    public void OnPlatform()
+    {
+        if (rigid.velocity.y <= 0)
+        {
+            jumpCount = jumpCountMax;
+        }
+    }
+
+    public void SetBindness(bool type)
+    {
+        if (type == true)
+            sightAnomaly = true;
+        else
+            sightAnomaly = false;
+    }
+
+    public void Smoke()
+    {
+        Collider[] colliders;
+        Vector3 center = transform.position;
+        float radius = 1.0f;
+
+        colliders = Physics.OverlapSphere(center, radius);
+
+        bool isSmoking = false;
+        foreach (Collider coll in colliders)
+        {
+            if (coll.transform.tag == "SMOKE")
+            {
+                isSmoking = true;
+                SetBindness(true);
+                break;
+            }
+        }
+        if (isSmoking == false)
+        {
+            SetBindness(false);
+        }
+    }
+
 }
