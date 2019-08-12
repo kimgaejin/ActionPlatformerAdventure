2fd03533cadade8f2d4daae89fa07fcaa54c8463
SonOfMondrian <modern5607@naver.com>
2019-08-12 18:16:28 +0900

    미끄러운지대 연출 구현
    옆으로 퍼지는 연출 구현

--- Assets/Resources/Scripts/OilAlgorithm.cs
+++ Assets/Resources/Scripts/OilAlgorithm.cs
@@ -2,42 +2,129 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Oilsplash : MonoBehaviour
+public class OilAlgorithm : MonoBehaviour
 {
 
     public int totalsplash;
-    public GameObject[] Oilcolliders;
+    //public GameObject[] Oilcolliders;
     public GameObject Oil;
 
-    // Start is called before the first frame update
+    Vector3 LtempVec;
+    Vector3 RtempVec;
+    
     void Start()
     {
-        Oilcolliders = new GameObject[4];
         Oil = Resources.Load<GameObject>("Prefabs/Oil");
-        totalsplash = 20;
-        for(int i=0;i<4;i++)
-        {
-            Oilcolliders[i] = Oil.transform.GetChild(i).gameObject;
-        }
+        totalsplash = 3;
+        
     }
     void OnCollisionEnter(Collision other)
     {
-        if(other.collider.tag == "Platform")
+        if (other.gameObject.tag == "BLASTSOUND")
         {
-            Debug.Log("기름통이 바닥에 충돌");
-            transform.GetComponent<CapsuleCollider>().enabled = false;
+            Debug.Log("음폭탄이 기름통에 빢!");
+            Destroy(transform.GetComponent<HingeJoint>());
+            transform.GetComponent<CapsuleCollider>().isTrigger = true;
+            transform.parent = null;
         }
     }
-   
-    IEnumerator Splash()
+
+    void OnTriggerEnter(Collider other)
     {
-        while(true)
+        if (other.GetComponent<Collider>().tag == "Platform")
         {
 
+            Debug.Log("기름통이 바닥에 충돌");
+            transform.GetComponent<Rigidbody>().isKinematic = true;
+            transform.GetComponent<CapsuleCollider>().enabled = false;
+            transform.GetComponent<MeshRenderer>().enabled = false;
+
+            Vector3 oilvec = new Vector3(transform.position.x, other.transform.position.y + 1.01f, transform.position.z);
+            Instantiate(Oil, oilvec, Quaternion.identity);
 
 
-            yield return new WaitForSeconds(1);
+            LtempVec = oilvec;
+            RtempVec = oilvec;
+            StartCoroutine("Splash");
         }
+    }
+    //void sp(Collider other)
+    //{
+    //    Vector3 oilvec = new Vector3(transform.position.x, other.transform.position.y + 1.01f, transform.position.z);
+    //    Instantiate(Oil, oilvec, Quaternion.identity);
 
+    //    Vector3 LtempVec = oilvec;
+    //    Vector3 RtempVec = oilvec;
+    //    for (int i = totalsplash; i > 0; i--)       //번갈아가면서 생성하기위해.
+    //    {
+    //        if (LtempVec != Vector3.zero)
+    //            LtempVec = LSplash(LtempVec);
+
+    //        if (RtempVec != Vector3.zero)
+    //            RtempVec = RSplash(RtempVec);
+    //    }
+    //}
+
+    Vector3 LSplash(Vector3 oilvec)
+    {
+        Vector3 LeftVec = new Vector3(oilvec.x - 0.27f, oilvec.y, oilvec.z);
+        Collider[] Lcolls = Physics.OverlapBox(LeftVec - new Vector3(0.3f, 0, 0), new Vector3(0.1f, 0.1f, 0.1f));
+
+        if (Lcolls.Length == 0)
+            return Vector3.zero;
+        else
+        {
+            foreach (var i in Lcolls)
+            {
+                if (i.tag == "Platform")
+                {
+                    Debug.Log(i.name);
+                    Instantiate(Oil, LeftVec, Quaternion.identity);
+                    break;
+                }
+            }
+        }
+        return LeftVec;
+    }
+
+    Vector3 RSplash(Vector3 oilvec)
+    {
+        Vector3 RightVec = new Vector3(oilvec.x + 0.27f, oilvec.y, oilvec.z);
+        Collider[] Rcolls = Physics.OverlapBox(RightVec + new Vector3(0.3f, 0, 0), new Vector3(0.1f, 0.1f, 0.1f));
+
+        if (Rcolls.Length == 0)
+            return Vector3.zero;
+        else
+        {
+            foreach (var i in Rcolls)
+            {
+                if (i.tag == "Platform")
+                {
+                    Debug.Log(i.name);
+                    Instantiate(Oil, RightVec, Quaternion.identity);
+                    break;
+                }
+            }
+        }
+        return RightVec;
+    }
+
+    IEnumerator Splash()
+    {
+        while (true)
+        {
+            for (int i = totalsplash; i > 0; i--)       //번갈아가면서 생성하기위해.
+            {
+                Debug.Log("코루틴 진입");
+                if (LtempVec != Vector3.zero)
+                    LtempVec = LSplash(LtempVec);
+
+                if (RtempVec != Vector3.zero)
+                    RtempVec = RSplash(RtempVec);
+
+                yield return new WaitForSeconds(0.1f);
+            }
+            StopCoroutine("Splash");
+        }
     }
 }
