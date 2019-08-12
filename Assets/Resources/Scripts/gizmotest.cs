e77f9a4b69846ed2385bf12cc35e34577c8f7bc7
SonOfMondrian <modern5607@naver.com>
2019-08-13 05:33:27 +0900

    환풍구 구현중

---
+++ Assets/Resources/Scripts/gizmotest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gizmotest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position - new Vector3(0.3f, 0, 0), new Vector3(0.1f, 0.1f, 0.1f));
+    }
+}
