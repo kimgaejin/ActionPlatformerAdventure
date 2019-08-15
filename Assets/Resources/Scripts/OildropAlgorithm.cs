e4dc65708a5a57a174ae23fdb199466bbcfe754a
SonOfMondrian <modern5607@naver.com>
2019-08-16 04:28:04 +0900

    기름 흐른거 구현중
    구현중 낭떠러지에서 한방울만 떨어져서하는데 여러방울 떨어지는 문제 발견, 이 문제 해결해야함 ㅠ

--- Assets/Resources/Scripts/OildropAlgorithm.cs
+++ Assets/Resources/Scripts/OildropAlgorithm.cs
@@ -2,47 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OilAlgorithm : MonoBehaviour
+public class OildropAlgorithm : MonoBehaviour
 {
 
     public int totalsplash;
-    //public GameObject[] Oilcolliders;
     public GameObject Oil;
     public GameObject OilDrop;
-    public bool isOildrop;
-
     Vector3 LtempVec;
     Vector3 RtempVec;
-
+    // Start is called before the first frame update
     void Start()
     {
         Oil = Resources.Load<GameObject>("Prefabs/Oil2");
         OilDrop = Resources.Load<GameObject>("Prefabs/Oildrop");
-        totalsplash = 300;
-        isOildrop = false;
+        Invoke("ColliderON",0.4f);
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
     }
-    void OnCollisionEnter(Collision other)
+
+   
+    void ColliderON()
     {
-        if (other.gameObject.tag == "BLASTSOUND")
-        {
-            Debug.Log("음폭탄이 기름통에 빢!");
-            Destroy(transform.GetComponent<HingeJoint>());
-            transform.GetComponent<CapsuleCollider>().isTrigger = true;
-            transform.parent = null;
-        }
+        transform.Find("Drop").GetComponent<BoxCollider>().enabled = true;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Collider>().tag == "Platform")
         {
-            Debug.Log("기름통이 바닥에 충돌");
-            transform.GetComponent<Rigidbody>().isKinematic = true;
-
-            transform.GetComponent<CapsuleCollider>().enabled = false;
-            transform.GetComponent<MeshRenderer>().enabled = false;
+           
 
             Vector3 oilvec = new Vector3(transform.position.x, other.transform.position.y + 1.01f, transform.position.z);
             Instantiate(Oil, oilvec, Quaternion.identity);
@@ -105,15 +97,15 @@ public class OilAlgorithm : MonoBehaviour
                 }
             }
         }
-        
+
         return RightVec;
     }
-
     public void CreateOilDrop(Vector3 vec)     //기름이 퍼지면서 아래에 아무것도 없으면 기름방울이 떨어진다.
     {
         GameObject Oild = Instantiate(OilDrop, vec, Quaternion.identity);
         Oild.GetComponent<OildropAlgorithm>().totalsplash = totalsplash;
     }
+
 
     IEnumerator Splash()
     {
@@ -130,7 +122,7 @@ public class OilAlgorithm : MonoBehaviour
 
                 totalsplash = i;
                 yield return new WaitForSeconds(0.01f);
-                
+
             }
             StopCoroutine("Splash");
         }
