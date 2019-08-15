fc2929917d68dcb159fd0b22be86e59e7c0b6c79
SonOfMondrian <modern5607@naver.com>
2019-08-15 19:38:51 +0900

    가시지형 구현
    -음폭탄과의 충돌 구현
    -가시지형은 플레이어와 충돌X
    -바닥에 떨어짐

--- Assets/Resources/Scripts/BlastsoundAlgorithm.cs
+++ Assets/Resources/Scripts/BlastsoundAlgorithm.cs
@@ -24,13 +24,22 @@ public class BlastsoundAlgorithm : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision coll)
+    void OnCollisionEnter(Collision coll)       //현재 천장에 있는 가시와 충돌하면 이 함수와 아래 트리거 함수가 같이 호출됨. 지금은 트리거가 먼저 호출후, 0.몇초후에 이 함수가 호출되는 문제. 일단 임시방편..
     {
-        //Debug.Log("충돌해서 섬광격발");
+        Debug.Log("collision충돌해서 섬광격발");
         Flash();
         Destroy(this.gameObject);
+    }
 
-
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SPIKE")        //충돌처리 임시
+        {
+            Debug.Log("Trigger 충돌해서 섬광격발");
+            Flash();
+            Destroy(this.gameObject);
+        }
+        
     }
 
     void Flash()
---
+++ Assets/Resources/Scripts/SpikeAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeAlgorithm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "BLASTSOUND")
+        {
+            Debug.Log("가시지형과 음폭탄 충돌");
+            for(int i=0;i<transform.childCount;i++)
+            {
+
+                //transform.GetChild(i).gameObject.AddComponent<Rigidbody>();
+                
+                transform.GetChild(i).GetComponent<Rigidbody>().isKinematic = false;
+
+            }
+            Invoke("CollidersOn", 0.1f);    //음폭탄과 충돌시 가시가 위로 튀는 현상때문에 시간차를 둬 콜리더 활성.
+        }
+
+    }
+    void CollidersOn()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+            transform.GetChild(i).GetComponent<CapsuleCollider>().enabled = true;
+    }
+}
